60cd633a8276284b503808c852409a1c926d056c
Naufal Azka <askanaufal74@gmail.com>
2024-11-14 09:02:37 +0700

    done renamed class

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //ADD COMMENT    
 function addDiscussion() {
-  let name = document.getElementById("name--value").value;
-  let discussion = document.getElementById("comment--value").value;
+  let name = document.getElementById("name-value").value;
+  let discussion = document.getElementById("comment-value").value;
   
   //KALO name atau discussion KOSONG ALERT 
   if (name === "" || discussion === "") {
@@ -9,12 +9,12 @@ function addDiscussion() {
     return
   }
       
-  const discussionListPost = document.getElementById("discussion--container--list--post");
+  const discussionListPost = document.getElementById("discussion__container__list__post");
   const divList = document.createElement("div");
   discussionListPost.append(divList);
           
   const divCard = document.createElement('div');
-  divCard.className = "container--flex--row"
+  divCard.className = "flex__row"
 
   const profilePhoto = document.createElement("img");
   profilePhoto.src = ('src/misc/unknown_icon.png');
@@ -32,8 +32,8 @@ function addDiscussion() {
   divList.append(divCard);
   divList.append(discussionValue);
   divList.append(lineBreak);
-  document.getElementById("name--value").value = "";
-  document.getElementById("comment--value").value = "";
+  document.getElementById("name-value").value = "";
+  document.getElementById("comment-value").value = "";
 }
 
 
@@ -227,20 +227,20 @@ document.addEventListener("DOMContentLoaded", () => {
 //   }
 // }
 const shipFilters = {
-  "BB__BC__BBV__BM__IXm": { title: "BB/BC/BBV/BM/IXm", types: ["Battleship", "Aviation Battleship"] },
-  "CV__CVL": { title: "CV/CVL", types: ["Aircraft Carrier"] },
-  "CA__CB": { title: "CA/CB", types: ["Heavy Cruiser", "Large Cruiser"] },
-  "CL__IXv": { title: "CL/IXv", types: ["Light Cruiser"] },
-  "DD__DDG": { title: "DD/DDG", types: ["Destroyer"] }
+  "BB-BC-BBV-BM-IXm": { title: "BB/BC/BBV/BM/IXm", types: ["Battleship", "Aviation Battleship"] },
+  "CV-CVL": { title: "CV/CVL", types: ["Aircraft Carrier"] },
+  "CA-CB": { title: "CA/CB", types: ["Heavy Cruiser", "Large Cruiser"] },
+  "CL-IXv": { title: "CL/IXv", types: ["Light Cruiser"] },
+  "DD-DDG": { title: "DD/DDG", types: ["Destroyer"] }
 };
 
 
-document.querySelectorAll(".shiplist .shiplist--container .shiplist--container--list--class li").forEach((li) => {
+document.querySelectorAll(".shiplist .shiplist__container .shiplist__container__list__class li").forEach((li) => {
   li.addEventListener("click", () => {
     const filter = li.getAttribute("data-filter");
-    filterShip(filter);
+    filterShip(filter)
 
-    document.querySelectorAll(".shiplist .shiplist--container .shiplist--container--list--class li").forEach((item) => {
+    document.querySelectorAll(".shiplist .shiplist__container .shiplist__container__list__class li").forEach((item) => {
       item.classList.remove("active__filter")
     });
     li.classList.add("active__filter");
@@ -249,40 +249,41 @@ document.querySelectorAll(".shiplist .shiplist--container .shiplist--container--
 
 
 function filterShip(filter) {
-  const shiplistContainer = document.getElementById("shiplist--container--ship");
-  const conta = document.getElementById("shiplist--container");
+  const shiplistContainer = document.getElementById("shiplist-container");
+  const shiplistContainerShip = document.getElementById("shiplist-container-ship");
+  
+  shiplistContainerShip.remove();
 
-  const divConta = document.createElement("div");
-  divConta.id = "shiplist--container--ship";
-  conta.appendChild(divConta);
+  const divShiplistContainerShip = document.createElement("div");
+  divShiplistContainerShip.id = "shiplist-container-ship";
+  shiplistContainer.appendChild(divShiplistContainerShip)
 
-  const divTitle = document.createElement("h3");
-  divTitle.className = "text--h3";
+  const divShiplistContainerShipTitle = document.createElement("h3");
+  divShiplistContainerShipTitle.className = "text__h3";
 
-  const divFill = document.createElement("div");
-  divFill.classList.add("shiplist--container--ship", "container--flex--row");
+  const divShiplistContainerShipFill = document.createElement("div");
+  divShiplistContainerShipFill.classList.add("shiplist__container__ship", "flex__row");
 
   const selectedFilter = shipFilters[filter];
-  divTitle.textContent = selectedFilter.title;
-  divConta.append(divTitle);
-  divConta.append(divFill);
+  divShiplistContainerShipTitle.textContent = selectedFilter.title;
+  divShiplistContainerShip.append(divShiplistContainerShipTitle);
+  divShiplistContainerShip.append(divShiplistContainerShipFill);
 
-  shiplistContainer.remove();
 
   shipJSON.forEach((ship) => {
     if (selectedFilter.types.includes(ship.type)) {
       const htmlContent = `
-        <div class="shiplist--container--ship--card">
-          <div class="shiplist--container--ship--card--img">
+        <div class="shiplist__container__ship__card">
+          <div class="shiplist__container__ship__card__img">
             <img src="${ship.image.potrait}" alt="${ship.name}" data-index="${ship.id}" draggable="false"/>
           </div>
-          <div class="shiplist--container--ship--card--title">
+          <div class="shiplist__container__ship__card__title">
             <h4>${ship.name}</h4>
           </div>
         </div>
       `;
 
-      divFill.insertAdjacentHTML("beforeend", htmlContent);
+      divShiplistContainerShipFill.insertAdjacentHTML("beforeend", htmlContent);
     }
   });
 
@@ -293,12 +294,11 @@ function filterShip(filter) {
 
 //CREATE CARD
 function createCard() {
-  const containerBB_BBV = document.getElementById("shipBB--BC--BBV--BM--IXm")
-  const containerCV = document.getElementById("shipCV--CVL")
-  const containerCB_CA = document.getElementById("shipCA--CB");
-  const containerCL = document.getElementById("shipCL--IXv")
-  const containerDD = document.getElementById("shipDD--DDG")
-
+  const containerBB_BBV = document.getElementById("shipBB-BC-BBV-BM-IXm")
+  const containerCV = document.getElementById("shipCV-CVL")
+  const containerCB_CA = document.getElementById("shipCA-CB");
+  const containerCL = document.getElementById("shipCL-IXv")
+  const containerDD = document.getElementById("shipDD-DDG");
   shipJSON.forEach((ship) => {
     let container;
 
@@ -311,11 +311,11 @@ function createCard() {
 
     if (container) {
       const htmlContent = `
-        <div class="shiplist--container--ship--card">
-          <div class="shiplist--container--ship--card--img">
+        <div class="shiplist__container__ship__card">
+          <div class="shiplist__container__ship__card__img">
             <img src="${ship.image.potrait}" alt="${ship.name}" data-index="${ship.id}" draggable="false"/>
           </div>
-          <div class="shiplist--container--ship--card--title">
+          <div class="shiplist__container__ship__card__title">
             <h4>${ship.name}</h4>
           </div>
         </div>
@@ -507,336 +507,3 @@ function closeModalBox() {
   document.body.style.overflow = "auto";
   navBar.style.display = '';
 }
-
-
-// let shipPotraitBB = [];
-// let shipPotraitBBName = ["Alsace", "Bismarck Zwei","Friedrich der Große","Kearsage","Musashi","New Jersey","Sovetsky Soyuz","Ulrich von Hutten","Vanguard","Warspite"];
-
-// for (let i = 0; i < shipPotraitBBName.length; i++){
-//   shipPotraitBB.push(`src/ships/main/BB-BC-BBV-BM-IXm/potrait/shippotrait${i}.png`)
-// }
-
-// function createCardBB() {
-//   let htmlContent = "";
-//   const shiplistContainerShipBB = document.getElementById("shipBB");
-
-//   shipPotraitBB.forEach(function (img, i) {
-//     htmlContent +=
-//     `<div class="shiplist--container--ship--card">
-//         <div class="shiplist--container--ship--card--img">
-//           <img src="${img}" alt="" data-type="BB" data-index="${i + 0}" />
-//         </div>
-//         <div class="shiplist--container--ship--card--title">
-//           <h4 class="text--h4">${shipPotraitBBName[i]}</h4>
-//         </div>
-//     </div>`;
-//   });
-
-//   shiplistContainerShipBB.innerHTML = htmlContent;
-// }
-      
-// //CREATE CV-CVL CARD
-// let shipPotraitCV = [];
-// let shipPotraitCVName = ["Amagi(CV)","Admiral Nakhimov","Hakuryuu","Implacable","Shinano","Yorktown II"];
-    
-// for (let i = 0; i < shipPotraitCVName.length; i++){
-//   shipPotraitCV.push(`src/ships/main/CV-CVL/potrait/shippotrait${i}.png`);
-// }
-      
-// function createCardCV() {
-//   let htmlContent = "";
-//   const shiplistContainerShipCV = document.getElementById("shipCV");
-        
-//   shipPotraitCV.forEach(function (img, i) {
-//   htmlContent +=
-//   `<div class="shiplist--container--ship--card">
-//     <div class="shiplist--container--ship--card--img">
-//       <img src="${img}" alt="" data-type="CV" data-index="${i + 0}" />
-//     </div>
-//     <div class="shiplist--container--ship--card--title">
-//       <h4 class="text--h4">${shipPotraitCVName[i]}</h4>
-//     </div>
-//   </div>`;
-//   });
-// shiplistContainerShipCV.innerHTML = htmlContent;
-// }
-//     createCardCV();
-    
-    
-//     //CREATE CA-CB CARD
-//     let shipPotraitCA = [];
-//     let shipPotraitCAName = ["Ägir","Azuma","Brest","Drake","Guam","Hindenburg","Kronshtadt","Napoli","Unzen"];
-    
-//     for (let i = 0; i < shipPotraitCAName.length; i++){
-//       shipPotraitCA.push(`src/ships/vanguard/CA-CB/potrait/shippotrait${i}.png`);
-//     }
-    
-//     function createCardCA() {
-//       let htmlContent = "";
-//       const shiplistContainerShipCA = document.getElementById("shipCA");
-      
-//       shipPotraitCA.forEach(function (img, i) {
-//         htmlContent +=
-//         `<div class="shiplist--container--ship--card">
-//         <div class="shiplist--container--ship--card--img">
-//         <img src="${img}" alt="" data-type="CA" data-index="${i + 0}" />
-//         </div>
-//         <div class="shiplist--container--ship--card--title">
-//         <h4 class="text--h4">${shipPotraitCAName[i]}</h4>
-//         </div>
-//         </div>`;
-//       });
-      
-//       shiplistContainerShipCA.innerHTML = htmlContent;
-//     }
-//     createCardCA();
-    
-    
-//     //CREATE CL CARD
-//     let shipPotraitCL = [];
-//     let shipPotraitCLName = ["Plymouth","San Diego"];
-    
-//     for (let i = 0; i < shipPotraitCLName.length; i++){
-//       shipPotraitCL.push(`src/ships/vanguard/CL/potrait/shippotrait${i}.png`);
-//     }
-    
-//     function createCardCL() {
-//       let htmlContent = "";
-//       const shiplistContainerShipCL = document.getElementById("shipCL");
-      
-//       shipPotraitCL.forEach(function (img, i) {
-//         htmlContent +=
-//         `<div class="shiplist--container--ship--card">
-//         <div class="shiplist--container--ship--card--img">
-//         <img src="${img}" alt="" data-type="CL" data-index="${i + 0}" />
-//         </div>
-//         <div class="shiplist--container--ship--card--title">
-//         <h4 class="text--h4">${shipPotraitCLName[i]}</h4>
-//         </div>
-//         </div>`;
-//       });
-      
-//       shiplistContainerShipCL.innerHTML = htmlContent;
-//     }
-//     createCardCL();
-    
-    
-//     //CREATE DD CARD
-//     let shipPotraitDD = [];
-//     let shipPotraitDDName = ["Eldrigde","Laffey II","Mogador","Shimakaze","Yuudachi"];
-    
-//     for (let i = 0; i < shipPotraitDDName.length; i++){
-//       shipPotraitDD.push(`src/ships/vanguard/DD/potrait/shippotrait${i}.png`);
-//     }
-    
-//     function createCardDD() {
-//       let htmlContent = "";
-//       const shiplistContainerShipDD = document.getElementById("shipDD");
-      
-//       shipPotraitDD.forEach(function (img, i) {
-//         htmlContent +=
-//         `<div class="shiplist--container--ship--card">
-//         <div class="shiplist--container--ship--card--img">
-//         <img src="${img}" alt="" data-type="DD" data-index="${i + 0}" />
-//             </div>
-//             <div class="shiplist--container--ship--card--title">
-//             <h4 class="text--h4" data-index="${i + 0}">${shipPotraitDDName[i]}</h4>
-//             </div>
-//             </div>`;
-//           });
-          
-//           shiplistContainerShipDD.innerHTML = htmlContent;
-//         }
-        
-//         createCardDD();
-//         createCardBB();
-
-// // AKHIRNYA JADI JUGA ALHAMDULILLAH THANK YOU ChatGPT-4o
-// const navBar = document.getElementById("navbar");
-// const modalBox = document.createElement("div");
-// modalBox.id = "modal";
-// modalBox.className = "modal";
-// modalBox.style.display = "none";  // Set awal display menjadi none
-// document.body.appendChild(modalBox);
-
-// function openModalBox(type, imgIndex) {
-//   // Tentukan direktori gambar penuh berdasarkan tipe
-//   let fullImageSrc;
-//   let fullNameArray = [];
-  
-//   if (type === "BB") {
-//     fullImageSrc = `src/ships/main/BB-BC-BBV-BM-IXm/ship${imgIndex}.png`;
-//     fullNameArray = shipPotraitBBName;
-//   } else if (type === "CV") {
-//     fullImageSrc = `src/ships/main/CV-CVL/ship${imgIndex}.png`;
-//     fullNameArray = shipPotraitCVName;
-//   } else if (type === "CA") {
-//     fullImageSrc = `src/ships/vanguard/CA-CB/ship${imgIndex}.png`;
-//     fullNameArray = shipPotraitCAName;
-//   } else if (type === "CL") {
-//     fullImageSrc = `src/ships/vanguard/CL/ship${imgIndex}.png`;
-//     fullNameArray = shipPotraitCLName;
-//   } else if (type === "DD") {
-//     fullImageSrc = `src/ships/vanguard/DD/ship${imgIndex}.png`;
-//     fullNameArray = shipPotraitDDName;
-//   }
-//   const fullName = fullNameArray[parseInt(imgIndex)]
-
-//   const modalBoxContent = document.createElement("div");
-//   modalBoxContent.className = "modal-content";
-  
-//   const htmlContent = `
-//     <div class="modal--overlay">
-//       <div class="modal--container--control">
-//         <h1 class="close--modal" onclick="closeModalBox()">X</h1>  <!-- Tambah onclick untuk menutup modal -->
-//       </div>
-//     </div>
-//     <div class="modal--container container--flex--row">
-//       <div class="modal--container--table--left container--flex--column">
-//         <div class="card">
-//           <img src="${fullImageSrc}" alt="" />  <!-- Menggunakan path full image -->
-//         </div>
-//         <div class="table--ship--desc">
-//           <table border="1">
-//             <tbody>
-//               <tr>
-//                 <th colspan="2">Ship Identity</th>
-//               </tr>
-//               <tr>
-//                 <th>Name</th>
-//                 <th>${fullName}</th>
-//               </tr>
-//               <tr>
-//                 <th>Faction</th>
-//                 <th>Northern Parliament</th>
-//               </tr>
-//               <tr>
-//                 <th>Rarity</th>
-//                 <th>DR ★★★★★★</th>
-//               </tr>
-//               <tr>
-//                 <th>Classification</th>
-//                 <th>Aviation Battleship</th>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-//       </div>
-//       <div class="modal--container--table--right container--flex--column">
-//         <div class="table--ship--stat">
-//           <table border="1">
-//             <tbody>
-//               <tr>
-//                 <th class="stats">-</th>
-//                 <th class="hp">Health</th>
-//                 <th class="fp">Firepower</th>
-//                 <th class="trp">Torpedo</th>
-//                 <th class="avi">Aviation</th>
-//                 <th class="aa">Anti Air</th>
-//                 <th class="eva">Evasion</th>
-//                 <th class="at">Armor Type</th>
-//               </tr>
-//               <tr>
-//                 <th class="stats">Base</th>
-//                 <th class="hp">10000</th>
-//                 <th class="fp">100</th>
-//                 <th class="trp">100</th>
-//                 <th class="avi">100</th>
-//                 <th class="aa">100</th>
-//                 <th class="eva">100</th>
-//                 <th class="at" rowspan="4">Medium</th>
-//               </tr>
-//               <tr>
-//                 <th class="stats">Level 100</th>
-//                 <th class="hp">10000</th>
-//                 <th class="fp">100</th>
-//                 <th class="trp">100</th>
-//                 <th class="avi">100</th>
-//                 <th class="aa">100</th>
-//                 <th class="eva">100</th>
-//               </tr>
-//               <tr>
-//                 <th class="stats">Level 120</th>
-//                 <th class="hp">10000</th>
-//                 <th class="fp">100</th>
-//                 <th class="trp">100</th>
-//                 <th class="avi">100</th>
-//                 <th class="aa">100</th>
-//                 <th class="eva">100</th>
-//               </tr>
-//               <tr>
-//                 <th class="stats">Level 125</th>
-//                 <th class="hp">10000</th>
-//                 <th class="fp">100</th>
-//                 <th class="trp">100</th>
-//                 <th class="avi">100</th>
-//                 <th class="aa">100</th>
-//                 <th class="eva">100</th>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-//         <div class="table--ship--equippable">
-//           <table border="1">
-//             <tbody>
-//               <tr>
-//                 <th class="gear" colspan="4">Equippable</th>
-//               </tr>
-//               <tr>
-//                 <th class="slot">Slots</th>
-//                 <th class="eff">Efficiency</th>
-//                 <th class="equip">Equippable</th>
-//                 <th class="max">Max #</th>
-//               </tr>
-//               <tr>
-//                 <th class="slot">1</th>
-//                 <th class="eff">100% -> 100%</th>
-//                 <th class="equip">BB Main Gun</th>
-//                 <th class="max">1 -> 1</th>
-//               </tr>
-//               <tr>
-//                 <th class="slot">2</th>
-//                 <th class="eff">100% -> 100%</th>
-//                 <th class="equip">BB Main Gun</th>
-//                 <th class="max">1 -> 1</th>
-//               </tr>
-//               <tr>
-//                 <th class="slot">3</th>
-//                 <th class="eff">100% -> 100%</th>
-//                 <th class="equip">BB Main Gun</th>
-//                 <th class="max">1 -> 1</th>
-//               </tr>
-//               <tr>
-//                 <th class="slot">Augment</th>
-//                 <th class="eff">N/A</th>
-//                 <th class="equip">Hammer, Dual Sword</th>
-//                 <th class="max">1 -> 1</th>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-//       </div>
-//     </div>
-//   `;
-
-//   modalBoxContent.innerHTML = htmlContent;
-//   modalBox.innerHTML = '';
-//   modalBox.appendChild(modalBoxContent);
-//   modalBox.style.display = "flex";
-//   navBar.style.display = 'none';
-// }
-
-// function closeModalBox() {
-//   modalBox.style.display = "none";
-//   navBar.style.display = '';
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.querySelectorAll(".root .shiplist .shiplist--container .shiplist--container--ship .shiplist--container--ship--card .shiplist--container--ship--card--img img").forEach((img) => {
-//     img.addEventListener("click", () => {
-//       const type = img.getAttribute("data-type");
-//       const index = img.getAttribute("data-index");
-//       openModalBox(type, index);  // Mengirim tipe dan indeks gambar
-//     });
-//   });
-// });
